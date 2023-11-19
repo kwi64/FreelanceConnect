56e9d5b1a2043cf9f0f2e7e4b747b1fe7fccd334
Kithmal Wimalaratne <wimalaratnek@student.douglascollege.ca>
2023-11-19 07:16:44 -0800

    feat: AuthenticationController with login and register basic routing, and its styles and resource images added

---
+++ src/main/java/com/csis3275/controller/AuthenticationController.java
@@ -0,0 +1,21 @@
+package com.csis3275.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthenticationController {
+	
+	@GetMapping("/login")
+	public String login(Model model) {
+//		model.addAttribute("view", "login/login");
+//		model.addAttribute("script", true);
+		return "login/login";
+	}
+	
+	@GetMapping("/register")
+	public String register(Model model) {
+		return "register/register";
+	}
+}
